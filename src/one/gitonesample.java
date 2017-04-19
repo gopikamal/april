d1e064523362bb4c72a4c1dee7e0584f076fead6
gopikamal <gopikamal638@gmail.com>
2017-04-19 19:08:34 +0530

    add first testing java program Change-Id: Ie65e3556ac401088265406b7704c276b0d1971d0 Signed-off-by: gopikamal <gopikamal638@gmail.com>

--- src/one/gitonesample.java
+++ src/one/gitonesample.java
@@ -3,8 +3,9 @@ package one;
 public class gitonesample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub 
+		for(int i=0;i<=10;i++){
+   System.out.println("added for testing ");
 	}
-
+	}
 }
